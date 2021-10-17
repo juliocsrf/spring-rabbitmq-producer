3c95cbf6c5e2ff547fbf321518ed384ce1bfe4de
Julio Fonseca <juliocsrmf@gmail.com>
2021-10-17 17:55:14 -0300

    Criação das filas

---
+++ src/test/java/dev/juliofonsca/rabbitmq/RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juliofonsca.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
